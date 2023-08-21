b91e447bfaf3252f449e7b04da28012c59829246
wendigo295 <75575185+wendigo295@users.noreply.github.com>
2023-08-21 11:41:33 +0530

    Update podcast_frontend.py

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -37,17 +37,6 @@ def main():
         with col2:
             st.image(podcast_info['podcast_details']['episode_image'], caption="Podcast Cover", width=300, use_column_width=True)
 
-        # Display the podcast guest and their details in a side-by-side layout
-        col3, col4 = st.columns([3, 7])
-
-        with col3:
-            st.subheader("Podcast Guest")
-            st.write(podcast_info['podcast_guest']['name'])
-
-        with col4:
-            st.subheader("Podcast Guest Details")
-            st.write(podcast_info["podcast_guest"]['summary'])
-
         # Display the five key moments
         st.subheader("Key Moments")
         key_moments = podcast_info['podcast_highlights']
